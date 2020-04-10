5d8989e7e525acc9a0ab24d89d4a98d8b6629615
rjbultitude <richard.bultitude@gmail.com>
2020-04-10 16:24:37 +0100

    apply prettier fixes

--- app/index.js
+++ app/index.js
@@ -2,12 +2,12 @@ import p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
 import './global.css';
 
-const sketchFn = (p5Sketch) => {
+const sketchFn = p5Sketch => {
   const config = {
     playing: false,
     fft: null,
     grainSize: 10,
-    mouseInCanvas: false
+    mouseInCanvas: false,
   };
   // Form controls
   const waveControls = document.controls.wave;
@@ -37,7 +37,11 @@ const sketchFn = (p5Sketch) => {
 
   function constrainAndPlay() {
     // Constrain playback to canvas
-    const freq = p5Sketch.constrain(p5Sketch.map(p5Sketch.mouseX, 0, p5Sketch.width, 10, 2024), 10, 2024);
+    const freq = p5Sketch.constrain(
+      p5Sketch.map(p5Sketch.mouseX, 0, p5Sketch.width, 10, 2024),
+      10,
+      2024
+    );
     if (p5Sketch.mouseY < p5Sketch.height && p5Sketch.mouseY > 0) {
       config.mouseInCanvas = true;
     } else {
@@ -51,7 +55,7 @@ const sketchFn = (p5Sketch) => {
   function drawFreqs() {
     const spectrum = config.fft.analyze();
     p5Sketch.noStroke();
-    for (let i = 0; i< spectrum.length; i++){
+    for (let i = 0; i < spectrum.length; i++) {
       let r = p5Sketch.map(i, 0, spectrum.length, 50, 255);
       let b = p5Sketch.map(i, 0, spectrum.length, 255, 50);
       let x = p5Sketch.map(i, 0, spectrum.length, p5Sketch.width, 0);
@@ -63,23 +67,23 @@ const sketchFn = (p5Sketch) => {
 
   p5Sketch.preload = function preload() {
     osc = new p5.Oscillator(waveType);
-  }
+  };
 
   p5Sketch.setup = function setup() {
-    let cnv = p5Sketch.createCanvas(1920,1080);
+    let cnv = p5Sketch.createCanvas(1920, 1080);
     cnv.parent('wrapper');
     cnv.mouseClicked(togglePlay);
     setupWaveControls();
     setUpGrainControl();
     config.fft = new p5.FFT();
     osc.amp(0.2);
-  }
+  };
 
   p5Sketch.draw = function draw() {
     p5Sketch.background(0, 0, 0);
     constrainAndPlay();
     drawFreqs();
-  }
+  };
 
   function togglePlay() {
     if (config.playing) {
@@ -96,9 +100,9 @@ const sketchFn = (p5Sketch) => {
     setupWaveControls,
     setUpGrainControl,
     constrainAndPlay,
-    drawFreqs
+    drawFreqs,
   };
-}
+};
 
 const P5 = new p5(sketchFn);
 export default P5;
