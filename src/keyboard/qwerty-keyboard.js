111bdaa16ddf9a4e61fde924b8298fe988a6249d
rjbultitude <richard.bultitude@gmail.com>
2020-10-21 22:40:06 +0100

    fix bug with multiple document keydown listeners

--- src/keyboard/qwerty-keyboard.js
+++ src/keyboard/qwerty-keyboard.js
@@ -1,27 +1,24 @@
-import { ONESHOT, SUSTAIN, QWERTY } from '../utils/constants';
+import { ONESHOT, QWERTY } from '../utils/constants';
 import { playAndShowNote, stopAndHideNote } from './keyboard';
 import { getDefaultIntervals } from '../utils/utils';
 
 export function setQwertyEvents(config, updateAudioOutput) {
-  const intervals = getDefaultIntervals(config);
-  intervals.forEach((item, index) => {
-    document.addEventListener(
-      'keydown',
-      (e) => {
-        if (config.playing && config.playMode === ONESHOT) {
-          return;
-        }
-        if (e.key === QWERTY[index]) {
-          console.log('QWERTY[index]', QWERTY[index]);
-          playAndShowNote(config, index, updateAudioOutput);
-        }
-      },
-      false
-    );
-    document.addEventListener('keyup', (e) => {
-      if (e.key === QWERTY[index] && config.playMode === ONESHOT) {
-        stopAndHideNote(config, updateAudioOutput);
+  document.addEventListener(
+    'keydown',
+    (e) => {
+      if (config.playing && config.playMode === ONESHOT) {
+        return;
       }
-    });
+      if (QWERTY.includes(e.key)) {
+        const currentKeyindex = QWERTY.indexOf(e.key);
+        playAndShowNote(config, currentKeyindex, updateAudioOutput);
+      }
+    },
+    false
+  );
+  document.addEventListener('keyup', (e) => {
+    if (QWERTY.includes(e.key) && config.playMode === ONESHOT) {
+      stopAndHideNote(config, updateAudioOutput);
+    }
   });
 }
