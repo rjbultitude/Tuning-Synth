2c43a2279e767b33cee2cbedb0dc86db227814d4
rjbultitude <richard.bultitude@gmail.com>
2020-10-21 18:37:26 +0100

    Intial qwerty implementation

---
+++ src/keyboard/qwerty-keyboard.js
@@ -0,0 +1,24 @@
+import { ONESHOT, QWERTY } from '../utils/constants';
+import { playAndShowNote, stopAndHideNote } from './keyboard';
+import { getDefaultIntervals } from '../utils/utils';
+
+export function setQwertyEvents(config, updateAudioOutput) {
+  const intervals = getDefaultIntervals(config);
+  intervals.forEach((item, index) => {
+    document.addEventListener(
+      'keydown',
+      (e) => {
+        if (e.key === QWERTY[index]) {
+          console.log('QWERTY[index]', QWERTY[index]);
+          playAndShowNote(config, index, updateAudioOutput);
+        }
+      },
+      false
+    );
+    document.addEventListener('keyup', (e) => {
+      if (e.key === QWERTY[index] && config.playMode === ONESHOT) {
+        stopAndHideNote(config, updateAudioOutput);
+      }
+    });
+  });
+}
