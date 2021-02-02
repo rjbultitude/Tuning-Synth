4b169ac0edb0daef3e3b9be68e6fdaffc054c8ee
rjbultitude <richard.bultitude@gmail.com>
2021-02-02 16:53:30 +0000

    specs for applyTuningSystem

--- src/audio-controllers/freqi-controls.spec.js
+++ src/audio-controllers/freqi-controls.spec.js
@@ -4,6 +4,7 @@ import sinonChai from 'sinon-chai';
 chai.use(sinonChai);
 
 import * as freqiFreqs from '../freqi-freqs/freqi-freqs';
+import * as onScreenKeyboard from '../keyboard/on-screen-keyboard';
 import * as freqiCtrls from './freqi-controls';
 
 describe('setOscFreqToTuningSys', function() {
@@ -43,29 +44,88 @@ describe('setOscFreqToTuningSys', function() {
       }
     };
     this.fn = () => {};
+    this.setTuningSysNotesSpy = sinon.spy(freqiFreqs, 'setTuningSysNotes');
+  });
+  afterEach(function() {
+    this.setTuningSysNotesSpy.restore();
   });
   it('should call setTuningSysNotes', function () {
-    const spy = sinon.spy(freqiFreqs, 'setTuningSysNotes');
     freqiCtrls.setOscFreqToTuningSys(this.config, this.fn);
-    expect(spy).calledOnce;
-    spy.restore();
+    expect(this.setTuningSysNotesSpy).calledOnce;
   });
   it('should call setTuningSysNotes with config', function () {
-    const spy = sinon.spy(freqiFreqs, 'setTuningSysNotes');
     freqiCtrls.setOscFreqToTuningSys(this.config, this.fn);
-    expect(spy).calledOnceWith(this.config);
-    spy.restore();
+    expect(this.setTuningSysNotesSpy).calledOnceWith(this.config);
   });
   it('should osc freq when osc started is true', function () {
     const spy = sinon.spy(this.configOscStarted.osc, 'freq');
     freqiCtrls.setOscFreqToTuningSys(this.configOscStarted, this.fn);
-    expect(spy).calledOnce;
     spy.restore();
+    expect(spy).calledOnce;
   });
   it('should call callback', function () {
     const spy = sinon.spy(this, 'fn');
     freqiCtrls.setOscFreqToTuningSys(this.configOscStarted, this.fn);
-    expect(spy).calledOnce;
     spy.restore();
+    expect(spy).calledOnce;
+  });
+});
+
+describe('applyTuningSystem', function() {
+  this.beforeEach(function() {
+    this.event = {
+      target: {
+        value: 'eqTemp'
+      }
+    };
+    this.config = {
+      startFreq: 0,
+      currentFreq: 0,
+      selectedInterval: 0,
+      currentFreq: 400,
+      osc: {
+        freq: 200,
+        started: false,
+      },
+      selectedTuningSys: 'eqTemp',
+      tuningSysNotes: {
+        eqTemp: [200, 400]
+      },
+      freqiTuningSysMeta: {
+        eqTemp: {
+          shortName: 'equal temp',
+          intervalsInOctave: 2
+        }
+      },
+      intervalsRange: {
+        lower: 1,
+        upper: 2
+      },
+      keyboardButtons: [{
+        id: 'id_1',
+        style: {
+          boxShadow: ''
+        }
+      }]
+    };
+    this.fn = () => {};
+    this.setTuningSysNotesSpy = sinon.spy(freqiFreqs, 'setTuningSysNotes');
+    this.highlightOctavesSpy = sinon.spy(onScreenKeyboard, 'highlightOctaves');
+  });
+  afterEach(function() {
+    this.setTuningSysNotesSpy.restore();
+    this.highlightOctavesSpy.restore();
+  });
+  it('should set selectedTuningSys to event target value', function() {
+    freqiCtrls.applyTuningSystem(this.event, this.config, this.fn);
+    expect(this.config.selectedTuningSys).to.equal(this.event.target.value);
+  });
+  it('should call setOscFreqToTuningSys', function() {
+    freqiCtrls.applyTuningSystem(this.event, this.config, this.fn);
+    expect(this.setTuningSysNotesSpy).calledOnce;
+  });
+  it('should call setOscFreqToTuningSys', function() {
+    freqiCtrls.applyTuningSystem(this.event, this.config, this.fn);
+    expect(this.highlightOctavesSpy).calledOnce;
   });
 });
